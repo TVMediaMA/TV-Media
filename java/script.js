b7077b5c91845b4b4468eac1814c034f351205a1
TVMediaMA <tvmediama@gmail.com>
2025-05-03 19:27:54 -0400

    Make logo clickable and add hover effect

--- java/script.js
+++ java/script.js
@@ -4,3 +4,22 @@ const nav = document.querySelector('nav');
 mobileMenu.addEventListener('click', () => {
   nav.classList.toggle('show');
 });
+
+
+const items = document.querySelectorAll('.timeline-item');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    } else {
+      entry.target.classList.remove('animate'); // so it repeats when scrolling back
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+items.forEach(item => observer.observe(item));
+
+  
